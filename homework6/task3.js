17fa49b61ea8b21b5b4f045e363ea7f4b2aabc50
Inna Bushyna <butnytska@gmail.com>
2025-01-23 18:56:18 -0800

    Update homework6/task3.js
    Co-authored-by: Kwazimodd <55921352+Kwazimodd@users.noreply.github.com>

--- homework6/task3.js
+++ homework6/task3.js
@@ -17,7 +17,7 @@ checkOrder(2, 0);
 
 function checkOrder2 (available, ordered) {
     if (ordered === 0) {
-        console.log("Your order is empty.");
+        return "Your order is empty.";
     }
     else if (ordered > available) {
         console.log("Your order is too large, we don't have enough goods.");
