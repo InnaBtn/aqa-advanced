f1ef117473021ee5fffea8be8af3f645fc8892b0
Inna Bushyna <butnytska@gmail.com>
2025-01-23 18:56:23 -0800

    Update homework6/task3.js
    Co-authored-by: Kwazimodd <55921352+Kwazimodd@users.noreply.github.com>

--- homework6/task3.js
+++ homework6/task3.js
@@ -20,7 +20,7 @@ function checkOrder2 (available, ordered) {
         return "Your order is empty.";
     }
     else if (ordered > available) {
-        console.log("Your order is too large, we don't have enough goods.");
+        return "Your order is too large, we don't have enough goods.";
     }
     else console.log("Your order is accepted");
 }
