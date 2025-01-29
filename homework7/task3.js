72421d46078256357e796c68e14c058816a9c85b
Inna Bushyna <butnytska@gmail.com>
2025-01-28 19:59:12 -0800

    Updated code formatting in task3.js file.

--- homework7/task3.js
+++ homework7/task3.js
@@ -1,35 +1,35 @@
 function divide(numerator, denominator) {
     if (denominator === 0) {
         throw new Error("Division by zero is not allowed.");
-    }        
-    if (typeof numerator !== "number" || typeof denominator !== "number" ) {
+    }
+    if (typeof numerator !== "number" || typeof denominator !== "number") {
         throw new Error("Both arguments must be numbers.");
     }
     return numerator / denominator;
 }
 
-   try {
-        console.log(divide(10, 2));
-    } catch (error) {
-        console.error("Error occured:", error.message);
-    }
+try {
+    console.log(divide(10, 2));
+} catch (error) {
+    console.error("Error occured:", error.message);
+}
 
-   try {
-        console.log(divide(20, 0));
-    } catch (error) {
-        console.error("Error occured:", error.message);
-    }
+try {
+    console.log(divide(20, 0));
+} catch (error) {
+    console.error("Error occured:", error.message);
+}
 
-    try {
-        console.log(divide("20", 2));
-    } catch (error) {
-        console.error("Error occured:", error.message);
-    }
+try {
+    console.log(divide("20", 2));
+} catch (error) {
+    console.error("Error occured:", error.message);
+}
 
-    try {
-        console.log(divide(10, true));
-    } catch (error) {
-        console.error("Error occured:", error.message);
-    } finally {
-        console.log("Work is complete.");
-    }
+try {
+    console.log(divide(10, true));
+} catch (error) {
+    console.error("Error occured:", error.message);
+} finally {
+    console.log("Work is complete.");
+}
